4b8dab58c44c59856c31cae04b5cfa3f4c9cc5f4
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-09-12 10:05:11 +0530

    😎

---
+++ index.js
@@ -0,0 +1,11 @@
+addEventListener('fetch', event => {
+  event.respondWith(handle(event.request))
+})
+
+async function handle(request) {
+const url = new URL(request.url);
+ return new Response(request.headers.get("cf-connecting-ip") ,{
+  status: 200,
+  statusText: (request.headers.get("cf-connecting-ip"))
+  })
+}
